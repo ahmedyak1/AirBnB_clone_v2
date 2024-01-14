2ed7801a7ef9f823dc4dac005694e6c205b1c215
ahmed yakine <132367513+ahmedyak1@users.noreply.github.com>
2024-01-14 23:23:23 +0100

    add 100-clean_web_static.py

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -17,13 +17,13 @@ def do_clean(number=0):
     """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    archi = sorted(os.listdir("versions"))
+    [archi.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(a)) for a in archi]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        archi = run("ls -tr").split()
+        archi = [a for a in archives if "web_static_" in a]
+        [archi.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archi]
