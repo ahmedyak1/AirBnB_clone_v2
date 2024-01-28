3943bc9d15e68695cbb14fde1c01710ebb126de4
ahmedyak1 <ahmedyak1@github.com>
2024-01-28 23:53:06 +0000

    Fixing task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
 """program that launches a Flask online application"""
 
+from flask import Flask
+
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 @app.route('/')
 def index():
     """display "Hello HBNB!"
-
-    Returns:
-        str: text on the index page
+        str: text on index page
     """
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb')
 def hbnb_route():
-    """display "HBNB"
 
-    Returns:
-        str: text on the page
+    """display "HBNB"
+        str: text  page
     """
     return 'HBNB'
 
@@ -32,6 +31,23 @@ def c_route(text):
         text (str): text to be served on the page
 
     Returns:
-        str: text on the page
+        str: text  page
     """
     return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
+def python_route(text):
+    """display "Python", followed by the value of the text variable
+
+
+    Args:
+        text (str): text to be served on the page
+        str: text on page
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
