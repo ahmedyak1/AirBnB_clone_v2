5b603ee59bdc47adb657bf28c9af7226803ef3a8
ahmedyak1 <ahmedyak1@github.com>
2024-01-29 01:07:40 +0000

    state.pu

--- models/state.py
+++ models/state.py
@@ -1,35 +1,42 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+""" stattte Module for HBNB pro"""
+from os import environ
+
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+
+from models.base_model import Base, BaseModel
 import models
+
 from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    """ State CLLASS """
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if environ.get('HBNB_TYPE_STORAGE') == 'db':
+
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete-orphan",
+                              passive_deletes=True)
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if environ.get('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Return list of citites
+
+            
+            """
+            return [city for city in models.storage.all(
+                City).values() if city.state_id == self.id]
+
